ac281dc1e5ca61328918979f19af7393b58437d4
Zhuofan Li <zhuofanli17@gmail.com>
2023-04-28 12:14:12 -0700

    Added completed tasks folding

--- public/js/index.js
+++ public/js/index.js
@@ -189,6 +189,22 @@ document.addEventListener("DOMContentLoaded", async function(event) {
           choicePopUp.style.display = 'none';
         });
     });
+
+    // Completed Task Folding
+    var coll = document.getElementsByClassName("collapsible");
+    var j;
+
+    for (j = 0; j < coll.length; j++) {
+        coll[j].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
 });
 
 class Task {
